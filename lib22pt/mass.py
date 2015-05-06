f0dae33caa878a05d60a70febc54ab970e7df25a
Stepan Roucka <stepan@roucka.eu>
2015-05-06 13:35:50 +0200

    Improve input checking in mass.py

--- lib22pt/mass.py
+++ lib22pt/mass.py
@@ -3,12 +3,10 @@ import numpy as np
 class Mass:
     def __init__(self, fname):
         self.fname = fname
-        self.data = np.loadtxt(fname, skiprows=8)[1:-1, [0, 1, 6]]
-
         import re
         fr = open(fname)
 
-        for line in fr:
+        for lineno, line in enumerate(fr):
             if re.search("// Integration \(s\)*", line):
                 self.integration =\
                     float(re.search("// Integration \(s\) ([0-9.]+)*", line).group(1))
@@ -18,6 +16,15 @@ class Mass:
             if re.search("// 8P\. *", line):
                 self.pot_8P =\
                     float(line.split()[2])
+            if re.search("Mass*", line):
+                firstline = lineno
+        fr.close()
+
+        try:
+            self.data = np.loadtxt(fname, skiprows=firstline+2)[1:-1, [0, 1, 6]]
+        except StopIteration as e:
+            print("Mass: no data in file %s" % fname)
+            raise
         self.data[:,[1,2]] *= self.iterations
         #self.data[:,[1,2]] /= self.integration
         #self.data[:,0] += self.pot_8P
