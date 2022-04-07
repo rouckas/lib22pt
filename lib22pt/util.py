ba68423e1da76009cc95a1691c6082f92de80af6
Stepan Roucka <stepan@roucka.eu>
2022-04-07 11:45:26 +0200

    Add set_unit for setting plot units

--- lib22pt/util.py
+++ lib22pt/util.py
@@ -277,6 +277,16 @@ def plot_shifted(data, name, ax, xfun=lambda x:x, plotargs=dict(label="", color=
             **plotargs)
 
 
+def set_unit(axis, unit):
+    """Set units of a matplotlib axis. Usage: set_unit(ax1.yaxis, 1e-9) """
+    from matplotlib.ticker import FuncFormatter
+    axis.set_major_formatter(
+        FuncFormatter(
+            lambda x, pos: np.round(x/unit, decimals=3)
+        )
+    )
+
+
 def print_banner(text, ch='#', length=78):
     spaced_text = ' %s ' % text
     banner = spaced_text.center(length, ch)
