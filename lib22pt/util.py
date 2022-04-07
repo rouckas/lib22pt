0db2e702994dba1381a57b4d70ffcdf1a5d4ea84
Stepan Roucka <stepan@roucka.eu>
2022-04-07 11:42:23 +0200

    Add plotting utility functions

--- lib22pt/util.py
+++ lib22pt/util.py
@@ -250,6 +250,33 @@ def shift_T(df, Tcol, ncols, kcols, T_shift, T_e_low, T_e_high, sys_only=False):
     return
 
 
+def plot_avg_shifted(data_avg, name, ax, xfun=lambda x:x, plotargs=dict(label="", color="k", fmt="o", zorder=2), errorbar=True, err="err"):
+    if errorbar:
+
+        if err=="avgerr":
+            print("ERROR = ")
+            print(data_avg[name+"_shift_lo"+err])
+        ax.errorbar(xfun(data_avg["T22PT_shift"]), data_avg[name+"_shift"],
+            yerr=np.vstack((
+                data_avg[name+"_shift_lo"+err],
+                data_avg[name+"_shift_hi"+err]
+            )),
+            xerr=np.vstack((
+                -xfun(data_avg["T22PT_shift"]-data_avg["T22PT_shift_loerr"]) + xfun(data_avg["T22PT_shift"]),
+                 xfun(data_avg["T22PT_shift"]+data_avg["T22PT_shift_hierr"]) - xfun(data_avg["T22PT_shift"])
+                 )),
+            **plotargs)
+    else:
+        #fmt = plotargs.pop("fmt", "o")
+        ax.errorbar(xfun(data_avg["T22PT_shift"]), data_avg[name+"_shift"], **plotargs)
+
+
+def plot_shifted(data, name, ax, xfun=lambda x:x, plotargs=dict(label="", color="k", fmt="o", zorder=2)):
+    ax1.errorbar(xfun(data["T22PT_shift"]), data[name+"_shift"],
+            yerr=data[name+"_shift_err"],
+            **plotargs)
+
+
 def print_banner(text, ch='#', length=78):
     spaced_text = ' %s ' % text
     banner = spaced_text.center(length, ch)
